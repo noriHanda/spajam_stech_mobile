2718cf8a92ab1222a75821c2a6d2a49afd553f42
taisei <swiftmantaisei@gmail.com>
2022-08-20 16:51:48 +0900

    Firebase(Cloud Storageのセットアップ)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:spajam_stech/views/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
